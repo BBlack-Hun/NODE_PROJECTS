cc6b27aaec73996baca82eb305f6b0d820802cc7
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-11-11 00:00:52 +0900

    add get_taskOne

--- task_manager/controller/tasks/tasks.ctrl.js
+++ task_manager/controller/tasks/tasks.ctrl.js
@@ -19,7 +19,17 @@ exports.post_task = async (req, res) => {
 };
 
 exports.get_task = async (req, res) => {
-  res.send('get task');
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `no task with id : ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 exports.patch_task = async (req, res) => {
