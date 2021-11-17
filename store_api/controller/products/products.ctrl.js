f216353a73709ccd06ff73aa1703275faa68931d
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-11-18 00:16:08 +0900

    add async to all api

--- store_api/controller/products/products.ctrl.js
+++ store_api/controller/products/products.ctrl.js
@@ -1,11 +1,21 @@
 const asyncWrapper = require('../../middleWare/async');
 
-exports.get_products = asyncWrapper((req, res) => {});
+exports.get_products = asyncWrapper(async (req, res) => {
+  res.status(200).json({ msg: 'all products' });
+});
 
-exports.create_product = asyncWrapper((req, res) => {});
+exports.create_product = asyncWrapper(async (req, res) => {
+  res.status(200).json();
+});
 
-exports.get_product = asyncWrapper((req, res) => {});
+exports.get_product = asyncWrapper(async (req, res) => {
+  res.status(200).json();
+});
 
-exports.update_product = asyncWrapper((req, res) => {});
+exports.update_product = asyncWrapper(async (req, res) => {
+  res.status(200).json();
+});
 
-exports.delete_product = asyncWrapper((req, res) => {});
+exports.delete_product = asyncWrapper(async (req, res) => {
+  res.status(200).json();
+});
