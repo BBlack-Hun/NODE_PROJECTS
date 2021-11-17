7a96acc6b9d49894027e3333fe86809bcd9ff9bb
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-11-17 23:44:08 +0900

    add products index.js

--- store_api/controller/products/index.js
+++ store_api/controller/products/index.js
@@ -4,4 +4,12 @@ const ctrl = require('./products.ctrl');
 
 router.get('/', ctrl.get_products);
 
+router.post('/', ctrl.create_product);
+
+router.get('/:id', ctrl.get_product);
+
+router.put('/:id', ctrl.update_product);
+
+router.delete('/:id', ctrl.delete_product);
+
 module.exports = router;
