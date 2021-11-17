58dddf76e0371f3ccfe6f6c7f299de77928565b6
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-11-18 00:10:47 +0900

    add products.ctrl.js

--- store_api/controller/products/products.ctrl.js
+++ store_api/controller/products/products.ctrl.js
@@ -1,3 +1,11 @@
-exports.get_products = (req, res) => {
-  res.json('hi');
-};
+const asyncWrapper = require('../../middleWare/async');
+
+exports.get_products = asyncWrapper((req, res) => {});
+
+exports.create_product = asyncWrapper((req, res) => {});
+
+exports.get_product = asyncWrapper((req, res) => {});
+
+exports.update_product = asyncWrapper((req, res) => {});
+
+exports.delete_product = asyncWrapper((req, res) => {});
