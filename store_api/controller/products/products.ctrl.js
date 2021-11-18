0bdc0c444f86ce437c8d393db7a01f42f0be9482
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-11-18 23:16:56 +0900

    using page and QueryObject

--- store_api/controller/products/products.ctrl.js
+++ store_api/controller/products/products.ctrl.js
@@ -3,13 +3,20 @@ const asyncWrapper = require('../../middleWare/async');
 
 exports.get_products_static = asyncWrapper(async (req, res) => {
   const products = await Product.find({
-    name: 'vase table',
+    page: '2',
   });
   res.status(200).json({ products, hbHits: products.length });
 });
 
 exports.get_products = asyncWrapper(async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, hbHits: products.length });
 });
 
