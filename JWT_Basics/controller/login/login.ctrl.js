140d8213bac876b35ab8642b4d40d3e7e35d08bb
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-11-24 22:39:41 +0900

    add login logic on controller

--- JWT_Basics/controller/login/login.ctrl.js
+++ JWT_Basics/controller/login/login.ctrl.js
@@ -1,5 +1,15 @@
 const asyncWrapper = require('../../middleWare/async');
+const CustomApiError = require('../../error/coustom-error');
 
 exports.post_login = asyncWrapper((req, res) => {
+  const { username, password } = req.body;
+  // mongo
+  // joi
+  // check in the controller
+
+  if (!username || !password) {
+    throw new CustomApiError('Please provide email and password', 400);
+  }
+
   res.send('Fake Login/Register/Signup Route');
 });
