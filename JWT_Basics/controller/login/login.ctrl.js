4aa06c70decf71acc28cd76ee63985f366e4b221
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-11-24 22:48:46 +0900

    add jwt token

--- JWT_Basics/controller/login/login.ctrl.js
+++ JWT_Basics/controller/login/login.ctrl.js
@@ -1,15 +1,24 @@
 const asyncWrapper = require('../../middleWare/async');
 const CustomApiError = require('../../error/coustom-error');
+const jwt = require('jsonwebtoken');
 
 exports.post_login = asyncWrapper((req, res) => {
   const { username, password } = req.body;
-  // mongo
-  // joi
+  // mongoose validation
+  // Joi
   // check in the controller
 
   if (!username || !password) {
     throw new CustomApiError('Please provide email and password', 400);
   }
+  // just for demo, normally provided by DB!!!
+  const id = new Date().getDate();
 
-  res.send('Fake Login/Register/Signup Route');
+  // try to keep payload small, better experience for user
+  // just for demo, in production use long, complex and unguessable string value!!!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ msg: 'user created', token });
 });
