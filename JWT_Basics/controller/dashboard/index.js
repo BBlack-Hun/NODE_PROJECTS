12c89d46fde9bedb62cbe14fc7a2e3af4a05a46f
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-11-25 20:19:20 +0900

    add comments

--- JWT_Basics/controller/dashboard/index.js
+++ JWT_Basics/controller/dashboard/index.js
@@ -4,6 +4,7 @@ const ctrl = require('./dashboard.ctrl');
 
 const authMiddleware = require('../../middleWare/auth');
 
+// middle ware인 authMiddleware에서 검증이 끝난 후 넘어간다.
 router.get('/', authMiddleware, ctrl.get_dashboard);
 
 module.exports = router;
