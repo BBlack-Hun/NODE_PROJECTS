8bd69bae9c72361f9f4e4196744e283fa262a26a
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-01 21:57:33 +0900

    add jwt

--- JOB_API/controller/auth/auth.ctrl.js
+++ JOB_API/controller/auth/auth.ctrl.js
@@ -1,11 +1,19 @@
 const User = require('../../models/User');
 const asyncWrapper = require('../../middleware/async');
 const { StatusCodes } = require('http-status-codes');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.post_register = asyncWrapper(async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json(user);
+  const token = jwt.sign(
+    { userId: user._id, name: user.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d',
+      httpOnly: true,
+    },
+  );
+  res.status(StatusCodes.CREATED).json({ token });
 });
 
 exports.post_login = asyncWrapper(async (req, res) => {
