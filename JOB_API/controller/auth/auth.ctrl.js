7e1781fd41499a4442921e0627360f2e0cfc9081
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-01 21:40:00 +0900

    add register validation logic

--- JOB_API/controller/auth/auth.ctrl.js
+++ JOB_API/controller/auth/auth.ctrl.js
@@ -1,9 +1,17 @@
 const User = require('../../models/User');
 const asyncWrapper = require('../../middleware/async');
 const { StatusCodes } = require('http-status-codes');
+const bcrypt = require('bcryptjs');
 
 exports.post_register = asyncWrapper(async (req, res) => {
-  const user = await User.create({ ...req.body });
+  const { name, email, password } = req.body;
+
+  const salt = await bcrypt.getSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { name, email, password: hashedPassword };
+
+  const user = await User.create({ ...tempUser });
   res.status(StatusCodes.CREATED).json(user);
 });
 
