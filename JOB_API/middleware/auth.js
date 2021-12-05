74eca8bcdc3767744e4099338de09fe1b40b0234
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-05 16:13:23 +0900

    Job model

--- JOB_API/middleware/auth.js
+++ JOB_API/middleware/auth.js
@@ -12,9 +12,12 @@ const authenticationMiddleWare = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
 
-    // 아래 코드를 위와 같이 사용할 수 있다. -> 이유는 정리가 되지 않음 ^^
-    const user = User.findById(payload.id).select('-password');
-    req.user = user;
+    /**
+     * 아래 코드를 위와 같이 사용할 수 있다. -> 이유는 정리가 되지 않음 ^^
+     * select(-password)를 하여, 패스워드를 제외한 값을 req.user에 담아준다.
+     */
+    // const user = User.findById(payload.id).select('-password');
+    // req.user = user;
 
     // attach the user to the job routes
     req.user = { userId: payload.userId, name: payload.name };
