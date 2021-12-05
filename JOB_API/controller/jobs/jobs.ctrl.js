50e93a513dec17f00b6cfae76231d8dff633613a
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-05 16:26:57 +0900

    Job Route

--- JOB_API/controller/jobs/jobs.ctrl.js
+++ JOB_API/controller/jobs/jobs.ctrl.js
@@ -1,4 +1,7 @@
 const asyncWrapper = require('../../middleware/async');
+const Job = require('../../models/Job');
+const { StatusCodes } = require('http-status-codes');
+const BadRequestError = require('../../error/bad-request');
 
 exports.get_all_jobs = asyncWrapper(async (req, res) => {
   res.send('get all jobs');
@@ -9,7 +12,9 @@ exports.get_job = asyncWrapper(async (req, res) => {
 });
 
 exports.post_create_job = asyncWrapper(async (req, res) => {
-  res.send(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ job });
 });
 
 exports.update_job = asyncWrapper(async (req, res) => {
