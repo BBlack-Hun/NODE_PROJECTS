3f2f038da9351f9cca984031c0f0e4b9a50e7c01
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-09 20:26:22 +0900

    Cast Error

--- JOB_API/app.js
+++ JOB_API/app.js
@@ -75,6 +75,7 @@ class App {
       //   return res.status(err.statusCode).json({ msg: err.message });
       // }
       if (err.name === 'ValidationError') {
+        console.log(Object.values(err.errors));
         customError.msg = Object.values(err.errors)
           .map((item) => item.message)
           .join(',');
@@ -85,6 +86,10 @@ class App {
           err.keyValue,
         )} field, please choose another value`;
         customError.statusCode = 400;
+      }
+      if (err.name === 'CastError') {
+        customError.msg = `No item found with id : ${err.value}`;
+        customError.statusCode = 404;
       }
       // res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       //   err: err,
