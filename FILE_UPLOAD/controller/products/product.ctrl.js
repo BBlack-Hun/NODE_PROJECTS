0c4146f21477f7b5f0316b9e94d243151d1051bf
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-12 23:12:58 +0900

    Create Prodcuts

--- FILE_UPLOAD/controller/products/product.ctrl.js
+++ FILE_UPLOAD/controller/products/product.ctrl.js
@@ -3,7 +3,9 @@ const { StatusCodes } = require('http-status-codes');
 const asyncWrapper = require('../../middleware/async');
 
 exports.post_create_product = asyncWrapper(async (req, res) => {
-  res.send('create product');
+  console.log(req.body);
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json(product);
 });
 
 exports.get_all_products = asyncWrapper(async (req, res) => {
