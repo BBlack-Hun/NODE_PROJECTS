7e5c4caefa36f27e9c2f89661f23c816e4b17871
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-12 22:46:33 +0900

    initial Product model

---
+++ FILE_UPLOAD/models/Product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide name!'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please provide price!'],
+    },
+    image: {
+      type: String,
+      required: [true, 'Please provide Image!'],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('Product', ProductSchema);
