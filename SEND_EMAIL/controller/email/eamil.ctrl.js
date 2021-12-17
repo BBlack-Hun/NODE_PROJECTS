5608a54146fdc6f2e4c9de58678218070d8160e6
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-17 23:54:52 +0900

    nodemailer init

--- SEND_EMAIL/controller/email/eamil.ctrl.js
+++ SEND_EMAIL/controller/email/eamil.ctrl.js
@@ -1,5 +1,7 @@
+const nodemailer = require('nodemailer');
 const asyncWrapper = require('../../middleware/async');
 
 exports.get_send_email = asyncWrapper(async (req, res) => {
+  let testAccount = nodemailer.createTestAccount();
   res.send('send email func');
 });
