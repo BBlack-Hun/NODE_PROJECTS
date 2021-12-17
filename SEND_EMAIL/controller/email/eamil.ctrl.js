9ae9a4e6663fbc2b689cabad666c44acba1d7dfe
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-17 23:50:41 +0900

    updade send logic

--- SEND_EMAIL/controller/email/eamil.ctrl.js
+++ SEND_EMAIL/controller/email/eamil.ctrl.js
@@ -1,5 +1,5 @@
 const asyncWrapper = require('../../middleware/async');
 
 exports.get_send_email = asyncWrapper(async (req, res) => {
-  res.send('send email');
+  res.send('send email func');
 });
