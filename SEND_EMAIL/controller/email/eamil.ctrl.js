7ba2e6968b8ca47e9a2b906d5816efc20976c110
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-17 23:59:40 +0900

    send email test on ethereal

--- SEND_EMAIL/controller/email/eamil.ctrl.js
+++ SEND_EMAIL/controller/email/eamil.ctrl.js
@@ -3,5 +3,20 @@ const asyncWrapper = require('../../middleware/async');
 
 exports.get_send_email = asyncWrapper(async (req, res) => {
   let testAccount = nodemailer.createTestAccount();
-  res.send('send email func');
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'conner.sipes37@ethereal.email',
+      pass: 'mqHmB6Kn8rTXe7wA4y',
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: `"coding addict" <codingddict@gmail.com>`,
+    to: 'bar@example.com',
+    subject: 'hello',
+    html: '<h2>Sending on nodejs</h2>',
+  });
+  res.json(info);
 });
