d94f017100553fb5537761b1a27744f8b4e2ec80
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-21 23:12:08 +0900

    add stripe controller

--- STRIPE_PAYMENT/controller/stripe/index.js
+++ STRIPE_PAYMENT/controller/stripe/index.js
@@ -2,4 +2,6 @@ const { Router } = require('express');
 const router = Router();
 const ctrl = require('./stripe.ctrl');
 
+router.post('/stripe', ctrl.post_stripe);
+
 module.exports = router;
--- STRIPE_PAYMENT/controller/stripe/stripe.ctrl.js
+++ STRIPE_PAYMENT/controller/stripe/stripe.ctrl.js
@@ -1,1 +1,6 @@
 const asyncWrapper = require('../../middleware/async');
+
+exports.post_stripe = asyncWrapper(async (req, res) => {
+  console.log(req.body);
+  res.send('stripe route');
+});
