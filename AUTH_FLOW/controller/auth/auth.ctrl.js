ac17ea3cbf3c323b30abffb28f27889b65503f9e
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-27 23:23:43 +0900

    create user

--- AUTH_FLOW/controller/auth/auth.ctrl.js
+++ AUTH_FLOW/controller/auth/auth.ctrl.js
@@ -1,7 +1,11 @@
+const User = require('../../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomAPIError = require('../../errors');
 const asyncWrapper = require('../../middleware/async');
 
 exports.post_register = asyncWrapper(async (req, res) => {
-  res.send('register user');
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 });
 
 exports.post_login = asyncWrapper(async (req, res) => {
