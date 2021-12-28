358fa63950b574e87b26bd4aa1dd902b96782805
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-12-28 23:30:18 +0900

    add password hash

--- AUTH_FLOW/models/User.js
+++ AUTH_FLOW/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = mongoose.Schema(
   {
@@ -31,5 +33,16 @@ const UserSchema = mongoose.Schema(
   },
   { timestamps: true },
 );
+
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
 
 module.exports = mongoose.model('User', UserSchema);
