966e59b894b3c5bc215a9afc1b9edd82aabe5c78
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-01-07 23:43:47 +0900

    upate init user index.js

--- AUTH_FLOW/controller/user/index.js
+++ AUTH_FLOW/controller/user/index.js
@@ -2,4 +2,10 @@ const { Router } = require('express');
 const router = Router();
 const ctrl = require('./user.ctrl');
 
+router.get('/', ctrl.get_AllUsers);
+router.get('/showMe', ctrl.get_ShowCurrentUser);
+router.post('/updateUser', ctrl.patch_updateUser);
+router.post('/updateUserPassword', ctrl.patch_updateUserPassword);
+router.get('/:id', ctrl.get_SingleUser);
+
 module.exports = router;
