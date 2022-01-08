cdad9971d089129e412d2838aa057d5fb25df260
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-01-08 10:13:59 +0900

    GetAllUsers and GetSingleUser

--- AUTH_FLOW/controller/user/user.ctrl.js
+++ AUTH_FLOW/controller/user/user.ctrl.js
@@ -1,11 +1,20 @@
 const asyncWrapper = require('../../middleware/async');
+const User = require('../../models/User');
+const CustomError = require('../../errors');
+const { StatusCodes } = require('http-status-codes');
 
 exports.get_AllUsers = asyncWrapper(async (req, res) => {
-  res.send('get all users route');
+  const users = await User.find({ role: 'user' }).select('-password');
+  res.status(StatusCodes.OK).json({ users, count: users.length });
 });
 
 exports.get_SingleUser = asyncWrapper(async (req, res) => {
-  res.send(req.params);
+  const user = await User.findOne({ _id: req.params.id }).select('-password');
+
+  if (!user) {
+    throw new CustomError.notFoundError(`No user with id : ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 });
 
 exports.get_ShowCurrentUser = asyncWrapper(async (req, res) => {
