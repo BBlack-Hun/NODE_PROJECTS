dd92f782a2d9646660b84e50266193679d9c8003
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-01-26 22:43:35 +0900

    Review Routes Structure

--- AUTH_FLOW/controller/review/index.js
+++ AUTH_FLOW/controller/review/index.js
@@ -1,5 +1,15 @@
 const { Router } = require('express');
 const router = Router();
 const ctrl = require('./review.ctrl');
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../../middleware/authentication');
+
+router.post('/', authenticateUser, ctrl.post_createReview);
+router.get('/', authenticateUser, ctrl.get_AllReviews);
+router.get('/:id', ctrl.get_SingleReview);
+router.patch('/:id', authenticateUser, ctrl.update_Review);
+router.delete('/:id', authenticateUser, ctrl.delete_Review);
 
 module.exports = router;
--- AUTH_FLOW/controller/review/review.ctrl.js
+++ AUTH_FLOW/controller/review/review.ctrl.js
@@ -2,3 +2,23 @@ const Review = require('../../models/Review');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../../errors');
 const asyncWrapper = require('../../middleware/async');
+
+exports.post_createReview = asyncWrapper(async (req, res) => {
+  res.send('create review');
+});
+
+exports.get_AllReviews = asyncWrapper(async (req, res) => {
+  res.send('create review');
+});
+
+exports.get_SingleReview = asyncWrapper(async (req, res) => {
+  res.send('create review');
+});
+
+exports.update_Review = asyncWrapper(async (req, res) => {
+  res.send('create review');
+});
+
+exports.delete_Review = asyncWrapper(async (req, res) => {
+  res.send('create review');
+});
