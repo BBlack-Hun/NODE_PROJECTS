b90a846fba0231c6b3ecb6568fbed8a232c0f069
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-02-23 23:45:59 +0900

    send multiple Cookies

--- AUTH_FLOW/utils/jwt.js
+++ AUTH_FLOW/utils/jwt.js
@@ -2,29 +2,50 @@ const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes');
 
 const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME,
-  });
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
   return token;
 };
 
 // 리턴을 바로 시키려면,, {}로 감싸지 말것..
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = ({ res, user }) => {
-  const token = createJWT({ payload: user });
+const attachCookiesToResponse = ({ res, user, refreshToken }) => {
+  const accessTokenJWT = createJWT({ payload: { user } });
+  const refreshTokenJWT = createJWT({ payload: { user, refreshToken } });
 
   const oneDay = 1000 * 60 * 60 * 24;
 
-  res.cookie('token', token, {
+  res.cookie('accessToken', accessTokenJWT, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
+    maxAge: 1000,
+  });
+
+  res.cookie('refreshToken', refreshTokenJWT, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+    expires: new Date(Date.now() + oneDay),
   });
 
   // res.status(StatusCodes.CREATED).json({ user });
 };
+
+// const attachSingleCookieToResponse = ({ res, user }) => {
+//   const token = createJWT({ payload: user });
+
+//   const oneDay = 1000 * 60 * 60 * 24;
+
+//   res.cookie('token', token, {
+//     httpOnly: true,
+//     expires: new Date(Date.now() + oneDay),
+//     secure: process.env.NODE_ENV === 'production',
+//     signed: true,
+//   });
+
+//   // res.status(StatusCodes.CREATED).json({ user });
+// };
 
 module.exports = {
   createJWT,
