5987b7303f92d12bab0d6b0b1976a0d0cd3c6917
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-03-01 22:40:21 +0900

    forget password controller

--- AUTH_FLOW/controller/auth/auth.ctrl.js
+++ AUTH_FLOW/controller/auth/auth.ctrl.js
@@ -132,8 +132,29 @@ exports.delete_logout = asyncWrapper(async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 });
 
-exports.forget_Password = asyncWrapper(async (req, res) => {
-  res.send('forget password');
+exports.forgot_Password = asyncWrapper(async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    throw new CustomError.badRequestError('Email already exists');
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    const passwordToken = crypto.randomBytes(70).toString('hex');
+    // send email
+
+    const tenMinutes = 1000 * 60 * 10;
+    const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);
+    user.passwordToken = passwordToken;
+    user.passwordTokenExpirationDate = passwordTokenExpirationDate;
+    await user.save();
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: 'Please check your email for reset password link' });
 });
 
 exports.reset_Password = asyncWrapper(async (req, res) => {
