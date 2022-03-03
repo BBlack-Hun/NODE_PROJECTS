499430872f4eb04d97f1abffc6e200132257f318
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-03-03 23:26:28 +0900

    로그인 오류 수정 및 기타, 에러 수정

--- AUTH_FLOW/utils/sendResetPasswordEmail.js
+++ AUTH_FLOW/utils/sendResetPasswordEmail.js
@@ -2,14 +2,15 @@ const sendEmail = require('./sendEmail');
 const asyncWrapper = require('../middleware/async');
 
 const sendResetPasswordEmail = asyncWrapper(
-  async (name, email, token, origin) => {
+  async ({ name, email, token, origin }) => {
     const resetURL = `${origin}/user/reset-password?token=${token}&email=${email}`;
-    const message = `<p>Please reset password by clicking on following link : <a href="${resetURL}>Rest Password</a>"</p>`;
+
+    const message = `<p>Please reset password by clicking on following link : <a href=${resetURL}>Reset Password</a></p>`;
     return sendEmail({
       to: email,
-      subject: 'Reset password',
-      html: `<h4>Hello, ${name}</h4>
-      ${message}`,
+      subject: 'Reset Password',
+      html: `<h4> Hello, ${name}</h4>
+        ${message}`,
     });
   },
 );
